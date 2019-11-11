3d027fdc701e4819b769973929335fc6ed85a133
7077 <7077@mait-13-13.local>
2019-11-11 16:35:59 +1100

    Initial Commit

---
+++ HelloObjectiveCTesting/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloObjectiveCTesting
+//
+//  Created by 7077 on 11/11/19.
+//  Copyright © 2019 7077. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
